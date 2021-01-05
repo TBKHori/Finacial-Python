0b199bf4df39edbd0a481deefb83f3207b9b8e96
Telvin Roymen <tmaromeo5@gmail.com>
2021-01-05 21:14:01 +0300

    Ex2

--- Loading stock price data as DF.py
+++ Loading stock price data as DF.py
@@ -1,10 +1,16 @@
+ 
+# Ex1
+
 #import the package "Pandas" into Jupyter Notebook
 import pandas as pd
 
 #We import the stock data of Facebook into Jupyter Notebook. The CSV file is located in the folder called "Data" in your Workspace
+
+#Loading Facebook stock data
 #We then name the DataFrame name as 'fb'
 fb = pd.DataFrame.from_csv('../data/facebook.csv')
 
+#Loading Microsoft stock data
 ms = pd.DataFrame.from_csv('../data/microsoft.csv')
 
 # run this cell to ensure Microsoft's stock data is imported
@@ -13,3 +19,15 @@ print(ms.iloc[0, 0])
 46.73
 
 #As seen on: https://hub.coursera-apps.org:443/connect/sharedthedxiky?forceRefresh=false&path=%2Fnotebooks%2Fweek+1%2FImport+data.ipynb
+
+# Ex2
+
+#import the packages "Pandas" and "MatPlotLib" into Jupyter Notebook
+import pandas as pd
+import matplotlib.pyplot as plt
+#%matplotlib inline     ... Note supposed to be commented out...
+
+#import Facebook's stock data
+fb = pd.DataFrame.from_csv('../data/facebook.csv')
+
+print(fb.head())
